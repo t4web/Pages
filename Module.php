6e067d6c5071f1a306b659ffe3e274ffa94f5205
maxgu <gulturyan@gmail.com>
2014-12-01 17:27:05 +0200

    add Module.php

---
+++ Module.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Page;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\Mvc\Controller\ControllerManager;
+use Page\Controller\CreateController;
+
+class Module implements AutoloaderProviderInterface
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\', '/', __NAMESPACE__),
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+            ),
+        );
+    }
+
+    public function getControllerConfig()
+    {
+        return array(
+            'factories' => array(
+                'Page\Controller\CreateController' => function (ControllerManager $cm) {
+                    $sl = $cm->getServiceLocator();
+                    return new CreateController(
+                        //$sl->get('Authentication\Service')
+                    );
+                },
+            )
+        );
+    }
+}

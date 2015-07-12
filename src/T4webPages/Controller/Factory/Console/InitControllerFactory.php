7b4a0f515afa9b57f3b38b3a5bd2355f60a8ca64
maxgu <maxgu@t4web.com.ua>
2015-07-12 17:00:20 +0300

    Scrutinizer Fixes

--- src/T4webPages/Controller/Factory/Console/InitControllerFactory.php
+++ src/T4webPages/Controller/Factory/Console/InitControllerFactory.php
@@ -3,12 +3,12 @@
 namespace T4webPages\Controller\Factory\Console;
 
 use Zend\ServiceManager\FactoryInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\Mvc\Controller\ControllerManager;
 use T4webPages\Controller\Console\InitController;
 
 class InitControllerFactory implements FactoryInterface {
 
-    public function createService(ServiceLocatorInterface $serviceLocator) {
+    public function createService(ControllerManager $serviceLocator) {
         $serviceManager = $serviceLocator->getServiceLocator();
         return new InitController(
             $serviceManager->get('Zend\Db\Adapter\Adapter')
